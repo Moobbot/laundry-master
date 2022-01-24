afdf42a40e93b5c509621ca037dfe2aceca24039
Moobbot <ngotam24082001@gmai.com>
2022-01-24 23:21:36 +0700

    Edit load

--- reuse/menu.php
+++ reuse/menu.php
@@ -1,3 +1,15 @@
+    <!-- ? Preloader Start -->
+    <div id="preloader-active">
+        <div class="preloader d-flex align-items-center justify-content-center">
+            <div class="preloader-inner position-relative">
+                <div class="preloader-circle"></div>
+                <div class="preloader-img pere-text">
+                    <img src="assets/img/logo/logo.png" alt="" style="width: 110px; height: 50px;">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Preloader End -->
     <header>
         <!-- Header Start -->
         <div class="header-area">
