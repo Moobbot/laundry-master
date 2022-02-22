1d0bcd2415f89c3282a576e8acbd92a8cd4069a9
Moobbot <ngotam24082001@gmai.com>
2022-02-22 20:58:18 +0700

    Update login

--- order/reuse/header_body.php
+++ order/reuse/header_body.php
@@ -33,7 +33,7 @@
                     </ul>
                 </div>
                 <div class="header-right">
-                    <a href="../order/login.php" class="header-btn2">Đặt ngay!</a>
+                    <a href="../order/login.html" class="header-btn2">Đặt ngay!</a>
                 </div>
             </nav>
         </div>
